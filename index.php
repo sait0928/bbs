0ae5ee479ec63631da8172f6289825fd53927ed4
y.saitou928 <y.saitou928@gmail.com>
2020-10-03 16:05:30 +0900

    大まかな形だけ完成

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+$dsn = 'mysql:dbname=bbs;host=localhost';
+$user = 'root';
+$pass = '';
+
+try {
+	$dbh = new PDO($dsn, $user, $pass);
+
+	if($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$text = htmlspecialchars(nl2br($_POST['text']));
+		$stmt = $dbh->prepare('INSERT INTO posts (post) VALUES (:post)');
+		$stmt->bindParam(':post', $text, PDO::PARAM_STR);
+		$stmt->execute();
+	}
+
+	$stmt = $dbh->query('SELECT * FROM posts ORDER BY id DESC');
+	$posts = $stmt->fetchAll();
+} catch(PDOException $e) {
+	$error = $e->getMessage();
+	echo $error;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -5,6 +33,26 @@
 		<title>掲示板</title>
 	</head>
 	<body>
-		<h1>test</h1>
+		<h1>掲示板</h1>
+		<div id="form">
+			<form action="" method="POST">
+				<textarea name="text" id="" cols="50" rows="5"></textarea>
+				<input type="submit" value="投稿">
+			</form>
+		</div>
+		<div id="posts">
+			<table>
+				<tr>
+					<th>投稿ID</th>
+					<th>投稿内容</th>
+				</tr>
+				<?php foreach((array)$posts as $post) : ?>
+				<tr>
+					<td><?php echo $post['id']; ?></td>
+					<td><?php echo $post['post']; ?></td>
+				</tr>
+				<?php endforeach; ?>
+			</table>
+		</div>
 	</body>
 </html>
