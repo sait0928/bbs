80d4c3e2ca33701612a1501edebab53bd9ace7ec
sait0928 <y.saitou928@gmail.com>
2020-10-08 20:49:10 +0900

    投稿を削除する機能

--- index.php
+++ index.php
@@ -11,10 +11,17 @@ try {
 	$dbh = new PDO($dsn, $user, $pass);
 
 	if($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$text = $_POST['text'];
-		$stmt = $dbh->prepare('INSERT INTO posts (post) VALUES (:post)');
-		$stmt->bindParam(':post', $text, PDO::PARAM_STR);
-		$stmt->execute();
+		if($_POST['process'] === 'insert') {
+			$text = $_POST['text'];
+			$stmt = $dbh->prepare('INSERT INTO posts (post) VALUES (:post)');
+			$stmt->bindParam(':post', $text, PDO::PARAM_STR);
+			$stmt->execute();
+		} else if($_POST['process'] === 'delete') {
+			$id = $_POST['id'];
+			$stmt = $dbh->prepare('DELETE FROM posts WHERE id=:id');
+			$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+			$stmt->execute();
+		}
 	}
 
 	$stmt = $dbh->query('SELECT * FROM posts ORDER BY id DESC');
@@ -36,6 +43,7 @@ try {
 		<div id="form">
 			<form action="" method="POST">
 				<textarea name="text" id="" cols="50" rows="5"></textarea>
+				<input type="hidden" name="process" value="insert">
 				<input type="submit" value="投稿">
 			</form>
 		</div>
@@ -49,6 +57,13 @@ try {
 				<tr>
 					<td><?php echo $post['id']; ?></td>
 					<td><?php echo nl2br(htmlspecialchars($post['post'])); ?></td>
+					<td>
+						<form action="" method="POST">
+							<input type="hidden" name="id" value="<?php echo $post['id']; ?>">
+							<input type="hidden" name="process" value="delete">
+							<input type="submit" value="削除">
+						</form>
+					</td>
 				</tr>
 				<?php endforeach; ?>
 			</table>
