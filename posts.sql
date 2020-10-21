163aba22685a0beaebb013122f880240ccf8cd9e
sait0928 <y.saitou928@gmail.com>
2020-10-21 14:31:39 +0900

    sqlファイルの修正

--- posts.sql
+++ posts.sql
@@ -1,5 +1,5 @@
-mysql> create table bbs.posts(
-    -> id int auto_increment,
-    -> post varchar(255) not null,
-    -> primary key(id)
-    -> );
+create table posts(
+    id int auto_increment,
+    post varchar(255) not null,
+    primary key(id)
+);
