e37e77c6702ad29cd3ca37bbd9991a396bb4a7dd
sait0928 <y.saitou928@gmail.com>
2020-11-12 20:18:55 +0900

    deleteへのルート漏れを修正

--- public/index.php
+++ public/index.php
@@ -5,14 +5,15 @@ $path = $url['path'];
 
 const CONTROLLER_BASE = '../controllers';
 const ROUTES = [
-	'/' => '/index.php',
-	'/user_page' => '/user_page.php',
-	'/insert' => '/insert.php',
-	'/login_form' => '/login_form.php',
-	'/login' => '/login.php',
+	'/'              => '/index.php',
+	'/user_page'     => '/user_page.php',
+	'/insert'        => '/insert.php',
+	'/delete'        => '/delete.php',
+	'/login_form'    => '/login_form.php',
+	'/login'         => '/login.php',
 	'/register_form' => '/register_form.php',
-	'/register' => '/register.php',
-	'/logout' => '/logout.php',
+	'/register'      => '/register.php',
+	'/logout'        => '/logout.php',
 ];
 $route = ROUTES[$path] ?? '/404.php';
 include CONTROLLER_BASE . $route;
