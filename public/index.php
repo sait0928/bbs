dbd9b1ad4a3e68a1fae1ded98f98cb56f35490a4
sait0928 <y.saitou928@gmail.com>
2020-11-20 17:25:31 +0900

    Routing作成

--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 ini_set('display_errors', "On");
 error_reporting(E_ALL);
 
-include '../functions/routes.php';
+use Routing\Routing;
+
 spl_autoload_register(function (string $class_name) {
 	$file_path = '../' . str_replace('\\', '/', $class_name) . '.php';
 	include_once($file_path);
 });
 
-routing($_SERVER['REQUEST_URI']);
+$routing = new Routing();
+$routing->routing($_SERVER['REQUEST_URI']);
