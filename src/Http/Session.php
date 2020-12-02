818a534a50030f952d0c75f0754a967256ab3f92
sait0928 <y.saitou928@gmail.com>
2020-12-02 14:43:56 +0900

    Sessionにphpdoc記述

--- src/Http/Session.php
+++ src/Http/Session.php
@@ -1,13 +1,24 @@
 <?php
 namespace Http;
 
+/**
+ * sessionに関わる関数を扱うクラス
+ *
+ * @package Http
+ */
 class Session
 {
+	/**
+	 * session 開始
+	 */
 	public function start()
 	{
 		session_start();
 	}
 
+	/**
+	 * session 終了
+	 */
 	public function destroy()
 	{
 		session_destroy();
