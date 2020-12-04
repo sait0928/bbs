6817229f6e41937704e63a7c9def3a11686dbd37
sait0928 <y.saitou928@gmail.com>
2020-12-04 14:59:07 +0900

    UserPageControllerのテスト作成

--- src/Controller/UserPageController.php
+++ src/Controller/UserPageController.php
@@ -3,6 +3,7 @@ namespace Controller;
 
 use Http\Http;
 use Http\Session;
+use Model\User\Auth;
 use Model\User\SelectUser;
 use Model\Post\PostReader;
 use Model\Post\PostCounter;
@@ -17,40 +18,76 @@ use View\View;
  */
 class UserPageController
 {
+	private Session $session;
+	private Auth $auth;
+	private Http $http;
+	private SelectUser $select_user;
+	private PostReader $post_reader;
+	private PostCounter $post_counter;
+	private Pagination $pagination;
+	private View $view;
+
+	public function __construct(
+		Session $session,
+		Auth $auth,
+		Http $http,
+		SelectUser $select_user,
+		PostReader $post_reader,
+		PostCounter $post_counter,
+		Pagination $pagination,
+		View $view
+	) {
+		$this->session = $session;
+		$this->auth = $auth;
+		$this->http = $http;
+		$this->select_user = $select_user;
+		$this->post_reader = $post_reader;
+		$this->post_counter = $post_counter;
+		$this->pagination = $pagination;
+		$this->view = $view;
+	}
+
+	public static function createDefault()
+	{
+		return new self(
+			new Session(),
+			new Auth(new SelectUser()),
+			new Http(),
+			new SelectUser(),
+			new PostReader(),
+			new PostCounter(),
+			new Pagination(),
+			new View()
+		);
+	}
+
 	/**
 	 * GET通信で取得したuser_idを元に
 	 * そのユーザーが投稿した記事一覧を表示
 	 */
 	public function userPageAction(): void
 	{
-		$session = new Session();
-		$session->start();
+		$this->session->start();
 
-		if (!isset($_SESSION['user_id'])) {
-			$http = new Http();
-			$http->redirect('/login_form');
+		if (!$this->auth->isLoggedIn()) {
+			$this->http->redirect('/login_form');
 		}
 
-		$select_user = new SelectUser();
-		$user = $select_user->selectUserById($_GET['user_id']);
+		$user = $this->select_user->selectUserById($_GET['user_id']);
 		$name = $user->getUserName();
 
 		$page = $_GET['page'] ?? 1;
-		$post_reader = new PostReader();
-		$posts = $post_reader->selectUserPosts($page, $_GET['user_id']);
+		$posts = $this->post_reader->selectUserPosts($page, $_GET['user_id']);
 
-		$post_counter = new PostCounter();
-		$total_posts = $post_counter->countUserPosts($_GET['user_id']);
-		$pagination = new Pagination();
-		$pages = $pagination->countPages($total_posts);
+		$total_posts = $this->post_counter->countUserPosts($_GET['user_id']);
+		$pages = $this->pagination->countPages($total_posts);
 
 		$params = [
 			'name' => $name,
 			'posts' => $posts,
 			'pages' => $pages,
 		];
-		$view = new View();
-		$view->render('/user_page.php', $params);
+		$this->view->render('/user_page.php', $params);
 
 	}
 }
