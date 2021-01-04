7df43a8795eae4626bfd75beea43c31fd7948364
sait0928 <y.saitou928@gmail.com>
2021-01-04 16:26:08 +0900

    追加したメソッドのテストを記述

--- tests/Pagination/PaginationTest.php
+++ tests/Pagination/PaginationTest.php
@@ -17,4 +17,13 @@ class PaginationTest extends TestCase
 		$this->assertSame(2, $pagination->countPages(6));
 		$this->assertSame(3, $pagination->countPages(7));
 	}
+
+	public function testCreatePageLinksArray()
+	{
+		$pagination = new Pagination();
+
+		$this->assertSame([1, 2, 3, 4, 5], $pagination->createPageLinksArray(1, 10));
+		$this->assertSame([3, 4, 5, 6, 7], $pagination->createPageLinksArray(5, 10));
+		$this->assertSame([6, 7, 8, 9, 10], $pagination->createPageLinksArray(9, 10));
+	}
 }
