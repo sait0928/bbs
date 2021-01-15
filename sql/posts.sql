8c74638242f5ce8f952dfdb1f9cfc65cc1faac21
sait0928 <y.saitou928@gmail.com>
2021-01-15 13:24:39 +0900

    table 書き換え

--- sql/posts.sql
+++ sql/posts.sql
@@ -1,7 +1,10 @@
 create table posts(
     id int auto_increment,
     post varchar(255) not null,
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
     user_id int not null,
     primary key (id),
+    index (id),
     foreign key (user_id) references users (id)
 );
