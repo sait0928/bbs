7c9e42a26f870bd923a2dedb44b2c1d35ae1761b
sait0928 <y.saitou928@gmail.com>
2021-02-20 11:31:56 +0900

    labelは不要なので削除

--- resource/js/user_update_form.js
+++ resource/js/user_update_form.js
@@ -7,22 +7,14 @@ ReactDOM.render(
 		<div id="user-update-form">
 			<form action="/user_update" method="POST">
 				<p>変更したい項目のみ入力してください</p>
-				<label htmlFor="email">
-					ユーザ名を変更:
-					<input type="text" name="name" id="name" />
-				</label><br />
-				<label htmlFor="pass">
-					メールアドレスを変更:
-					<input type="email" name="email" id="email" />
-				</label><br />
-				<label htmlFor="pass">
-					パスワードを変更:
-					<input type="password" name="pass" id="pass" />
-				</label><br />
-				<label htmlFor="pass">
-					変更後パスワード再入力:
-					<input type="password" name="again" id="again" />
-				</label><br />
+				ユーザ名を変更:
+				<input type="text" name="name" id="name" /><br />
+				メールアドレスを変更:
+				<input type="email" name="email" id="email" /><br />
+				パスワードを変更:
+				<input type="password" name="pass" id="pass" /><br />
+				変更後パスワード再入力:
+				<input type="password" name="again" id="again" /><br />
 				<input type="hidden" name="csrf_token" value={params.csrf_token} />
 				<input type="submit" value="更新" />
 			</form>
