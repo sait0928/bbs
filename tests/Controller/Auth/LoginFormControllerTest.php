bcc15912101426fb5c0b4e65f06d915b1ccebb65
sait0928 <y.saitou928@gmail.com>
2021-02-22 00:24:48 +0900

    LoginFormControllerTest修正

--- tests/Controller/Auth/LoginFormControllerTest.php
+++ tests/Controller/Auth/LoginFormControllerTest.php
@@ -2,21 +2,34 @@
 
 namespace Controller\Auth;
 
+use Http\CsrfToken;
 use PHPUnit\Framework\TestCase;
-use View\View;
+use View\ReactView;
 
 class LoginFormControllerTest extends TestCase
 {
 	public function testLoginFormAction()
 	{
-		$view = $this->getMockBuilder(View::class)->getMock();
-		$view->expects($this->once())
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->once())
+			->method('set')
+		;
+
+		$csrf_token->expects($this->once())
+			->method('get')
+		;
+
+		$react_view = $this->getMockBuilder(ReactView::class)->getMock();
+		$react_view->expects($this->once())
 			->method('render')
 			->with('/login_form.php')
 		;
 
 		$login_form_controller = new LoginFormController(
-			$view
+			$csrf_token,
+			$react_view
 		);
 		$login_form_controller->loginFormAction();
 	}
