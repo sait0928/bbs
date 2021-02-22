d7ac21977eba92533a569c2d688f6d1c110b7c89
sait0928 <y.saitou928@gmail.com>
2021-02-22 16:50:45 +0900

    LoginControllerTest書き換え

--- tests/Controller/Auth/LoginControllerTest.php
+++ tests/Controller/Auth/LoginControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Controller\Auth;
 
+use Http\CsrfToken;
 use Http\Http;
 use Model\User\Auth;
 use PHPUnit\Framework\TestCase;
@@ -31,12 +32,21 @@ class LoginControllerTest extends TestCase
 			->willReturn(true)
 		;
 
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->once())
+			->method('get')
+		;
+
 		$_SERVER['REQUEST_METHOD'] = 'POST';
 		$_POST['email'] = 'hoge@hoge.co.jp';
 		$_POST['pass'] = 'password';
+		$_POST['csrf_token'] = '';
 		$login_controller = new LoginController(
 			$http,
-			$auth
+			$auth,
+			$csrf_token
 		);
 		$login_controller->loginAction();
 	}
@@ -66,10 +76,18 @@ class LoginControllerTest extends TestCase
 			->method('isLoggedIn')
 		;
 
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->never())
+			->method('get')
+		;
+
 		$_SERVER['REQUEST_METHOD'] = 'GET';
 		$login_controller = new LoginController(
 			$http,
-			$auth
+			$auth,
+			$csrf_token
 		);
 		$this->expectException(\Exception::class);
 		$this->expectErrorMessage('exit with redirect');
@@ -103,15 +121,66 @@ class LoginControllerTest extends TestCase
 			->willReturn(false)
 		;
 
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->once())
+			->method('get')
+		;
+
 		$_SERVER['REQUEST_METHOD'] = 'POST';
 		$_POST['email'] = 'hoge@hoge.co.jp';
 		$_POST['pass'] = 'password';
+		$_POST['csrf_token'] = '';
 		$login_controller = new LoginController(
 			$http,
-			$auth
+			$auth,
+			$csrf_token
 		);
 		$this->expectException(\Exception::class);
 		$this->expectErrorMessage('exit with redirect');
+		$login_controller->loginAction();
+	}
+
+	/**
+	 * トークン認証に失敗した場合のテスト
+	 */
+	public function testLoginAction_TokenMismatch()
+	{
+		$http = $this->getMockBuilder(Http::class)->getMock();
+		$http->expects($this->once())
+			->method('redirect')
+			->with('/login_form')
+			->willReturnCallback(function () {
+				throw new \Exception('exit with redirect');
+			})
+		;
+
+		$auth = $this->getMockBuilder(Auth::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$auth->expects($this->never())
+			->method('login')
+		;
+
+		$auth->expects($this->never())
+			->method('isLoggedIn')
+		;
+
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->once())
+			->method('get')
+		;
+
+		$_SERVER['REQUEST_METHOD'] = 'POST';
+		$_POST['csrf_token'] = 'token';
+		$login_controller = new LoginController(
+			$http,
+			$auth,
+			$csrf_token
+		);
 		$login_controller->loginAction();
 	}
 }
