7b4f9ce06571d1727dd609ac1dbe7013227be10c
sait0928 <y.saitou928@gmail.com>
2021-02-24 15:21:53 +0900

    UserUpdateFormControllerTest作成

---
+++ tests/Controller/Auth/UserUpdateFormControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controller\Auth;
+
+use Http\CsrfToken;
+use PHPUnit\Framework\TestCase;
+use View\ReactView;
+
+class UserUpdateFormControllerTest extends TestCase
+{
+	public function testUserUpdateFormAction()
+	{
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->once())
+			->method('get')
+			->willReturn('token')
+		;
+
+		$react_view = $this->getMockBuilder(ReactView::class)->getMock();
+		$react_view->expects($this->once())
+			->method('render')
+			->with('/user_update_form.php')
+		;
+
+		$user_update_form_controller = new UserUpdateFormController(
+			$csrf_token,
+			$react_view
+		);
+		$user_update_form_controller->userUpdateFormAction();
+	}
+}
