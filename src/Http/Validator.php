c964803718d2435de7c6597c58a70617975341c6
sait0928 <y.saitou928@gmail.com>
2021-03-03 15:59:28 +0900

    Validator作成

---
+++ src/Http/Validator.php
@@ -0,0 +1,27 @@
+<?php
+namespace Http;
+
+class Validator
+{
+	private Http $http;
+
+	public function __construct(
+		Http $http
+	) {
+		$this->http = $http;
+	}
+
+	/**
+	 * stringかどうか検証して違う場合はredirect
+	 *
+	 * @param mixed $param
+	 * @param string $url
+	 * @psalm-assert string $param
+	 */
+	public function validateString($param, string $url): void
+	{
+		if(!is_string($param)) {
+			$this->http->redirect($url);
+		}
+	}
+}
