9911bd4ae32cd08299f589919c64c258d4cb895f
sait0928 <y.saitou928@gmail.com>
2021-03-09 14:10:18 +0900

    InsertControllerTest修正

--- tests/Controller/Post/InsertControllerTest.php
+++ tests/Controller/Post/InsertControllerTest.php
@@ -4,6 +4,7 @@ namespace Controller\Post;
 use Http\CsrfToken;
 use Http\Http;
 use Http\Session;
+use Http\Validator;
 use Model\Post\PostWriter;
 use PHPUnit\Framework\TestCase;
 
@@ -34,6 +35,17 @@ class InsertControllerTest extends TestCase
 			->method('get')
 		;
 
+		$validator = $this->getMockBuilder(Validator::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$validator->expects($this->once())
+			->method('validateInt')
+			->with(1, '/logout');
+		$validator->expects($this->once())
+			->method('validateString')
+			->with('test', '/')
+		;
+
 		$post_writer = $this->getMockBuilder(PostWriter::class)
 			->disableOriginalConstructor()
 			->getMock();
@@ -49,6 +61,7 @@ class InsertControllerTest extends TestCase
 			$session,
 			$http,
 			$csrf_token,
+			$validator,
 			$post_writer
 		);
 		$insert_controller->insertAction();
@@ -80,6 +93,13 @@ class InsertControllerTest extends TestCase
 			->method('get')
 		;
 
+		$validator = $this->getMockBuilder(Validator::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$validator->expects($this->never())
+			->method('validateInt')
+		;
+
 		$post_writer = $this->getMockBuilder(PostWriter::class)
 			->disableOriginalConstructor()
 			->getMock();
@@ -92,6 +112,7 @@ class InsertControllerTest extends TestCase
 			$session,
 			$http,
 			$csrf_token,
+			$validator,
 			$post_writer
 		);
 		$this->expectException(\Exception::class);
@@ -125,6 +146,13 @@ class InsertControllerTest extends TestCase
 			->method('get')
 		;
 
+		$validator = $this->getMockBuilder(Validator::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$validator->expects($this->never())
+			->method('validateInt')
+		;
+
 		$post_writer = $this->getMockBuilder(PostWriter::class)
 			->disableOriginalConstructor()
 			->getMock();
@@ -138,6 +166,62 @@ class InsertControllerTest extends TestCase
 			$session,
 			$http,
 			$csrf_token,
+			$validator,
+			$post_writer
+		);
+		$this->expectException(\Exception::class);
+		$this->expectErrorMessage('exit with redirect');
+		$insert_controller->insertAction();
+	}
+
+	/**
+	 * 期待する値が得られなかった場合
+	 */
+	public function testInsertAction_ValidationFailure()
+	{
+		$session = $this->getMockBuilder(Session::class)->getMock();
+		$session->expects($this->once())
+			->method('get')
+			->willReturn(1)
+		;
+
+		$http = $this->getMockBuilder(Http::class)->getMock();
+		$http->expects($this->never())
+			->method('redirect')
+		;
+
+		$csrf_token = $this->getMockBuilder(CsrfToken::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$csrf_token->expects($this->once())
+			->method('get')
+		;
+
+		$validator = $this->getMockBuilder(Validator::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$validator->expects($this->once())
+			->method('validateInt')
+			->with(1, '/logout')
+			->willReturnCallback(function () {
+				throw new \Exception('exit with redirect');
+			})
+		;
+
+		$post_writer = $this->getMockBuilder(PostWriter::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$post_writer->expects($this->never())
+			->method('insert')
+		;
+
+		$_SERVER['REQUEST_METHOD'] = 'POST';
+		$_POST['csrf_token'] = '';
+		$insert_controller = new InsertController(
+			$session,
+			$http,
+			$csrf_token,
+			$validator,
 			$post_writer
 		);
 		$this->expectException(\Exception::class);
